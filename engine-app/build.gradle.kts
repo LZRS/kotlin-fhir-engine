8bd44770c3a63496ef790024f19533bdd42ade7e
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2026-06-24 13:20:19 +0300

    Remove includeBuild

--- engine-app/build.gradle.kts
+++ engine-app/build.gradle.kts
@@ -59,8 +59,7 @@ kotlin {
 
   sourceSets {
     commonMain.dependencies {
-//      implementation(project(":engine"))
-        implementation("dev.ohs.fhir:engine")
+      implementation(project(":engine"))
         implementation(libs.androidx.datastore.preferences)
       implementation(libs.kotlin.fhir)
       implementation(libs.kotlinx.coroutines.core)
